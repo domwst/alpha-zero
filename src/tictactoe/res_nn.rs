5b75c29b7f303a762cc3acd1e7eea9b351504a24
Oleg Shatov <oleq.shatov@gmail.com>
2026-08-31 13:38:56 +0200

    Move ResBlock into res_nn module

--- src/tictactoe/res_nn.rs
+++ src/tictactoe/res_nn.rs
@@ -8,12 +8,60 @@ use tch::{
     },
 };
 
-use crate::{
-    alpha_zero::{AlphaZeroNet, NetworkOutput},
-    util::ResBlock,
-};
+use crate::alpha_zero::{AlphaZeroNet, NetworkOutput};
 
 const CHANNELS: i64 = 32;
+
+#[derive(Debug)]
+pub struct ResBlock {
+    conv1: Conv2D,
+    bn1: BatchNorm,
+    conv2: Conv2D,
+    bn2: BatchNorm,
+}
+
+impl ResBlock {
+    pub fn new<'a, P: Borrow<Path<'a>>>(path: P, channels: i64, pad: i64) -> Self {
+        let ker = 2 * pad + 1;
+        let path = path.borrow();
+        Self {
+            conv1: conv2d(
+                path / "conv1",
+                channels,
+                channels,
+                ker,
+                ConvConfig {
+                    padding: pad,
+                    bias: false,
+                    ..Default::default()
+                },
+            ),
+            bn1: batch_norm2d(path / "bn1", channels, Default::default()),
+            conv2: conv2d(
+                path / "conv2",
+                channels,
+                channels,
+                ker,
+                ConvConfig {
+                    padding: pad,
+                    bias: false,
+                    ..Default::default()
+                },
+            ),
+            bn2: batch_norm2d(path / "bn2", channels, Default::default()),
+        }
+    }
+}
+
+impl ModuleT for ResBlock {
+    fn forward_t(&self, xs: &tch::Tensor, train: bool) -> tch::Tensor {
+        let out = self.conv1.forward_t(xs, train);
+        let out = self.bn1.forward_t(&out, train).relu();
+        let out = self.conv2.forward_t(&out, train);
+        let out = self.bn2.forward_t(&out, train);
+        (out + xs).relu()
+    }
+}
 
 fn value_head<'a, P: Borrow<Path<'a>>>(path: P) -> SequentialT {
     let path = path.borrow();
--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,5 @@
 mod atomic;
-mod res_block;
 mod timer;
 
 pub(crate) use atomic::*;
-pub use res_block::*;
 pub use timer::*;
